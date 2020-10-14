806dc6d99384850d0260a8b96ec854b23d5b6e34
Cooper Waite <cewaite814@gmail.com>
2020-10-14 12:35:15 -0700

    Final Push

--- src/edu/miracosta/cs113/MorseCodeTree.java
+++ src/edu/miracosta/cs113/MorseCodeTree.java
@@ -1,5 +1,8 @@
 package edu.miracosta.cs113;
 
+import java.io.*;
+import java.util.Scanner;
+
 /**
  * MorseCodeTree : A BinaryTree, with Nodes of type Character to represent each letter of the English alphabet,
  * and a means of traversal to be used to decipher Morse code.
@@ -7,6 +10,20 @@ package edu.miracosta.cs113;
  * @version 1.0
  */
 public class MorseCodeTree extends BinaryTree<Character> {
+
+    public static void main(String[] args)
+    {
+        MorseCodeTree mct = new MorseCodeTree();
+        try
+        {
+            System.out.println(mct.translateFromMorseCode("** *- -* --"));
+        }
+        catch (Exception e)
+        {
+            System.out.println("REEEEEE");
+        }
+
+    }
 
     // TODO:
     // Build this class, which includes the parent BinaryTree implementation in addition to
@@ -26,8 +43,92 @@ public class MorseCodeTree extends BinaryTree<Character> {
      * @param morseCode The given input representing letters in Morse code
      * @return a String representing the decoded values from morseCode
      */
-    public String translateFromMorseCode(String morseCode) {
-        return "";
+
+    public String translateFromMorseCode(String morseCode) throws Exception{
+        String message = "";
+        BinaryTree<Character> current = buildMorseCodeTree();
+
+        String[] splitted = morseCode.split(" ");
+
+        for (int j = 0; j < splitted.length; j++)
+        {
+            for (int i = 0; i < splitted[j].length(); i++) {
+                if (splitted[j].charAt(i) == '*' && current.getLeftSubtree() != null) {
+                    current = current.getLeftSubtree();
+                } else if (splitted[j].charAt(i) == '-' && current.getRightSubtree() != null) {
+                    current = current.getRightSubtree();
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            message += current.getData();
+            current = buildMorseCodeTree();
+        }
+        return message;
+    }
+
+//    public String readMorseCodeTree(BinaryTree<Character> character, String morseCode, String message)
+//    {
+//        for (int i = 0; i < morseCode.length(); i++)
+//        {
+//            if (morseCode.charAt(i) == '*')
+//            {
+//                if ()
+//                character = getLeftSubtree();
+//                readMorseCodeTree(character, morseCode.substring(1), message);
+//            }
+//            else if (morseCode.charAt(i) == '-')
+//            {
+//                character = getRightSubtree();
+//                readMorseCodeTree(character, morseCode.substring(1), message);
+//            }
+//            else
+//            {
+//                message += character.root.data;
+//            }
+//        }
+//        return message;
+//    }
+
+    public BinaryTree buildMorseCodeTree()
+    {
+        BinaryTree<Character> morseCodeTree = new BinaryTree<>(null, null, null);
+        try {
+            File myObj = new File("src/input.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                BinaryTree<Character> current = morseCodeTree;
+                String line = myReader.nextLine();
+                String[] splitted = line.split(" ");
+                Character letter = splitted[0].charAt(0);
+                String code = splitted[1];
+                for (int i = 0; i < code.length(); i++)
+                {
+                    if (code.charAt(i) == '*')
+                    {
+                        if (current.getLeftSubtree() == null) {
+                            current.setLeftSubtree(letter);
+                        }
+                        current = current.getLeftSubtree();
+                    }
+                    else if (code.charAt(i) == '-')
+                    {
+                        if (current.getRightSubtree() == null) {
+                            current.setRightSubtree(letter);
+                        }
+                        current = current.getRightSubtree();
+                    }
+                }
+//                current.root.data = letter;
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return morseCodeTree;
     }
 
 } // End of class MorseCodeTree
